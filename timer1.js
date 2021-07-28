898094752f1bddbfd784ae2e75dea15ba0b8d365
aphy18 <79496374+aphy18@users.noreply.github.com>
2021-07-27 21:34:28 -0400

    Update timer1.js

--- timer1.js
+++ timer1.js
@@ -1,18 +1,17 @@
 let arr = process.argv.slice(2)
 
 function timer(times) {
-    let array = times.map(Number)
-
-    for (let i = 0; i < array.length; i++) {
-        if(array[i] < 0 || array[i] === NaN) {
-            return array[i].splice(i, 1)
-        }
-            setTimeout(() => {
-            process.stdout.write('\x07');
-        }, array[i] * 100)
-    }
-
-}
+  let array = times.map(Number)
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] < 0 || array[i] === NaN) {
+      return array[i].splice(i, 1)
+    };
+  
+  setTimeout(() => {
+  process.stdout.write('\x07');
+  }, array[i] * 100)
+ };
+};
 
 
 timer(arr)
